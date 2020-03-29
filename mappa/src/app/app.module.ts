1933004e68837e9e7f0ede754b96c58813fb7e89
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2020-03-29 14:03:06 +0000

    punto01

--- mappa/src/app/app.module.ts
+++ mappa/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core'; //importa questa libreria
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Imposta qui le tue api key
+     AgmCoreModule.forRoot({apiKey: 'AIzaSyBOtW-5KsaDrzQ41dtkFjPceCEu6Ny-B-s'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
